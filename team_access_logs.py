053cd68eebabba27ad0081a4315443d12c2b6504
Jonathan Felder <felder@berkeley.edu>
2025-03-12 16:06:10 -0700

    First working version

--- team_access_logs.py
+++ team_access_logs.py
@@ -1,151 +1,129 @@
 #!/usr/bin/env python3
-
 import csv
 import json
-from time import sleep
-from datetime import datetime
-
-from api_token import token
-
+import sys
 import requests
+
+from time import sleep
+from datetime import datetime, timedelta
 from tqdm import trange
+from api_token import token
 
 
 def write_dicts_to_csv(filename, dictionaries):
-    field_names = dictionaries[0].keys()
-    with open(filename, 'w') as csvfile:
-        writer = csv.DictWriter(csvfile,
-                                fieldnames=field_names,
-                                quoting=csv.QUOTE_NONNUMERIC)
-        writer.writeheader()
-        for dictionary in dictionaries:
-            writer.writerow(dictionary)
+  if not dictionaries:
+    raise ValueError('Dictionary is empty!')
+
+  field_names = dictionaries[0].keys()
+  with open(filename, 'w') as csvfile:
+    writer = csv.DictWriter(csvfile, fieldnames=field_names, quoting=csv.QUOTE_NONNUMERIC)
+    writer.writeheader()
+ 
+    for dictionary in dictionaries:
+      writer.writerow(dictionary)
 
 
-def get_access_logs():
-    all_access_logs = []
-    url = 'https://slack.com/api/team.accessLogs'
-    headers = {"Authorization": f"Bearer {token}"}
-    params = {'count': 1000}  # 1000 logs per page is maximum
+def format_members(members):
+  members_for_csv = [];
+  
+  for member in members:
+    last_login_epoch = members[member]['last_login']
+    if last_login_epoch == 0:
+      last_login_date = "No logins found!"
+    else:
+      last_login_date = datetime.fromtimestamp(last_login_epoch).isoformat()
+      
+    members_for_csv.append({
+      'id': members[member]['id'],
+      'name': members[member]['name'],
+      'display_name': members[member]['profile']['display_name'],
+      'real_name': members[member]['profile']['real_name'],  
+      'title': members[member]['profile']['title'], 
+      'email': members[member]['profile']['email'],  
+      'last_login': last_login_date
+    })
+    
+  return members_for_csv
 
-    print(' Downloading pages …')
+
+def get_last_logins(members, earliest_epoch):
+  url = 'https://slack.com/api/team.accessLogs'
+  headers = {"Authorization": f"Bearer {token}"}
+  params = {'count': 1000}  # 1000 logs per page is maximum
+  
+  # oldest log entry
+  # params['before'] = 1444070161
+  
+  current_date = datetime.today();
+  current_epoch = int(current_date.timestamp())
+  
+  while True:
+    print("  Retrieving logs starting from", datetime.fromtimestamp(current_epoch).strftime('%c'), file=sys.stderr)
+    params['before'] = current_epoch
+    
     for page in trange(1, 101):  # 100 pages is maximum
-        params['page'] = page
-        res = requests.get(url, headers=headers, params=params)
-        res_data = res.json()
+      params['page'] = page
+      res = requests.get(url, headers=headers, params=params)
+      res_data = res.json()
 
-        if not res_data['ok']:
-            raise ValueError('Something went wrong.', res.url,
-                             res_data["error"])
-
-        all_access_logs.extend(res_data['logins'])
-
-        sleep(3)  # Limit for Tier 2 is 20 req/min
-
-    return all_access_logs
-
-
-def get_last_logins(access_logs):
-    last_logins = {}  # {user_id: last_login}
-    user_names = {}  # {user_id: user_name}
-    user_dicts = []  # all info ready for write to csv
-    for log in access_logs:
-        user_id = log['user_id']
-        user_name = log['username']
-        last_login = log['date_last']
-        if user_id not in last_logins:
-            last_logins[user_id] = last_login
-        else:
-            if last_login > last_logins[user_id]:
-                last_logins[user_id] = last_login
-
-        user_names[user_id] = user_name
-
-    for user_id, user_name in user_names.items():
-        now = datetime.now()
-        last_login_dtm = datetime.fromtimestamp(last_logins[user_id])
-        inactive = now - last_login_dtm
-        inactive_days = inactive.days
-        user_dicts.append({
-            'user_id': user_id,
-            'user_name': user_name,
-            'deleted': False,
-            'is_bot': False,
-            'email': "Not available",
-            'last_login': last_login_dtm.isoformat(),
-            'inactive_days': inactive_days
-        })
-
-    return user_dicts
+      if not res_data['ok']:
+       raise ValueError('Something went wrong.', res.url, res_data['error'])
+      elif not res_data['logins']:
+       return members
+    
+      for log in res_data['logins']:
+        current_epoch = log['date_first']
+        if current_epoch < earliest_epoch:
+          return members
+        elif log['user_id'] in members and members[log['user_id']]['last_login'] < log['date_last']:
+          members[log['user_id']]['last_login'] = log['date_last']
+          
+      sleep(3)  # Limit for Tier 2 is 20 req/min  
 
 
 def get_all_members():
-    url = 'https://slack.com/api/users.list'
-    headers = {"Authorization": f"Bearer {token}"}
-    #params = {'limit': 10}
-    print(' Downloading members …')
-    res = requests.get(url, headers=headers)
-    res_data = res.json()
-    #print(json.dumps(res_data, indent=4, sort_keys=True))
+  url = 'https://slack.com/api/users.list'
+  headers = {"Authorization": f"Bearer {token}"}
+  #params = {'limit': 10}
 
-    if not res_data['ok']:
-        raise ValueError('Something went wrong.', res.url, res_data["error"])
+  res = requests.get(url, headers=headers)
+  res_data = res.json()
 
-    return res_data["members"]
-
-
-def add_missing_members(last_logins, all_members):
-    """Extends info about last logins with members missing in access logs"""
-    existing_ids = [user["user_id"] for user in last_logins]
-
-    for member in all_members:
-        if member["id"] in existing_ids:
-            if member["deleted"] or member["is_bot"]:
-                for user in last_logins:
-                    if user["user_id"] == member["id"]:
-                        last_logins.remove(user)
-            else:
-                for user in last_logins:
-                    if user["user_id"] == member["id"]:
-                        x = last_logins.index(user)
-                        #print(json.dumps(member, indent=4, sort_keys=True))
-                        if "email" in member["profile"]:
-                            last_logins[x]["email"] = member["profile"][
-                                "email"]
-            continue
-        elif member["deleted"] or member["is_bot"]:
-            continue
-        else:
-            if "email" in member["profile"]:
-                last_logins.append({
-                    'user_id': member["id"],
-                    'user_name': member["name"],
-                    'deleted': member["deleted"],
-                    'is_bot': member["is_bot"],
-                    'email': member["profile"]["email"],
-                    'last_login': "unknown",
-                    'inactive_days': "Not available"
-                })
-            else:
-                last_logins.append({
-                    'user_id': member["id"],
-                    'user_name': member["name"],
-                    'deleted': member["deleted"],
-                    'is_bot': member["is_bot"],
-                    'email': "Not available",
-                    'last_login': "unknown",
-                    'inactive_days': "Not available"
-                })
+  if not res_data['ok']:
+    raise ValueError('Something went wrong.', res.url, res_data['error'])
+    
+  for member in list(res_data['members']):
+    if member['deleted'] or member['is_bot'] or member['id'] == "USLACKBOT":
+      res_data['members'].remove(member)
+    else:
+      x = res_data['members'].index(member)
+      res_data['members'][x]['last_login'] = 0
+  
+  members = {x['id']: x for x in res_data['members']}
+  return members
 
 
 def main():
-    access_logs = get_access_logs()
-    write_dicts_to_csv('raw_data.csv', access_logs)
-    last_logins = get_last_logins(access_logs)
-    members = get_all_members()
-    add_missing_members(last_logins, members)
-    write_dicts_to_csv('last_logins.csv', last_logins)
+  earliest_date = datetime.today() - timedelta(days=180)
+  earliest_epoch = int(earliest_date.timestamp())
+  
+  #with open("members_new.txt", 'r') as file:
+  #  members = json.load(file)
+  #file.close()
+  #print(json.dumps(members, indent=4, sort_keys=True))
+  
+  print("Downloading active non bot slack members …", file=sys.stderr)
+  members = get_all_members()
+  print(" ", len(members), "member(s) retrieved!", file=sys.stderr)
+  
+  print("Downloading access logs from now until", earliest_date, '…', file=sys.stderr)
+  members = get_last_logins(members, earliest_epoch)
+  print("Writing last login info to CSV …", file=sys.stderr)
+  
+  formatted_members = format_members(members)
+  write_dicts_to_csv('last_logins.csv', formatted_members)
+  print("  Done!", file=sys.stderr)
 
 
 if __name__ == '__main__':
-    main()
+  main()
